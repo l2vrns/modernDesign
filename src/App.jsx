655c63c4405fba6bc36ebabf7b0799dfb79a0d14
l2vrns <lorans0alkhalailah@gmail.com>
2024-10-04 18:55:06 +0300

    made components structure and nav bar

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Brand, CTA, Navbar } from './components';
+import { Footer, Blog, Possibility, Features, Header, WhatGPT3 } from './containers'
+import './App.css'
+
+const App = () => {
+    return (
+        <div className='App'>
+            <div className='gradient_bg'>
+                <Navbar />
+                <Header />
+            </div>
+            <Brand />
+            <WhatGPT3/>
+            <Features/>
+            <Possibility/>
+            <CTA/>
+            <Blog/>
+            <Footer/>
+        </div>
+    )
+}
+
+export default App
