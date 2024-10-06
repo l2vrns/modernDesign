620a8935269b165a3557ed0913f833493799ac69
l2vrns <lorans0alkhalailah@gmail.com>
2024-10-06 03:06:50 +0300

    made features component

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import './features.css'
 
-const Features = () => {
+const Features = ({title, text}) => {
   return (
-    <div>Features</div>
+    <div className='gpt3__features-container__feature'>
+      <div className="gpt3__features-container__feature-title">
+        <div />
+        <h1>{title}</h1>
+      </div>
+      <div className="gpt3__features-container__feature-text">
+        <p>{text}</p>
+      </div>
+    </div>
+
   )
 }
 
