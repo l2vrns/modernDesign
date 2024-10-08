88d07c8b32f24cc0b7299e4b15607998828c4322
l2vrns <lorans0alkhalailah@gmail.com>
2024-10-08 04:55:33 +0300

    done

---
+++ src/containers/blog/imports.jsx
@@ -0,0 +1,7 @@
+import Blog01 from "../../assets/22.png";
+import Blog02 from "../../assets/blog2.png";
+import Blog03 from "../../assets/blog03.png";
+import Blog04 from "../../assets/blog04.png";
+import Blog05 from "../../assets/blog05.png";
+
+export { Blog01, Blog02, Blog03, Blog04, Blog05 };
